b3edeef56d8a3735f63bf17ab36266c38c44ab52
suhpark <suhpark@gmarket.com>
2023-04-14 00:30:58 +0900

    컴파일 오류 수정

--- src/main/java/com/fastcampus/kotlinspring/todo/api/model/TodoResponse.kt
+++ src/main/java/com/fastcampus/kotlinspring/todo/api/model/TodoResponse.kt
@@ -9,14 +9,16 @@ data class TodoResponse (
     val description: String,
     val done: Boolean,
     val createdAt: LocalDateTime,
-    val updatedAt: LocalDateTime
+    val updatedAt: LocalDateTime? // non-null로 변경
 ) {
     companion object {
         fun of (todo: Todo?): TodoResponse {
             checkNotNull(todo) { "Todo is null" }
+            checkNotNull(todo.id) { "Todo Id is null" } // checkNotNull 사용
 
             return TodoResponse(
-                id = todo.id,
+                id = todo.id!!, // nullable type을 non-null 타입에 넣으려니 오류
+                // !! -> 단언 연산자 사용하거나
                 title = todo.title,
                 description = todo.description,
                 done = todo.done,
